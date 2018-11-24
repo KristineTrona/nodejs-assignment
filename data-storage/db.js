4b2b237276290f69d9c928e54e1c4c5c67e3e999
Kristine <kristinetrona@gmail.com>
2018-11-24 13:57:20 +0100

    api for the get request created

--- data-storage/db.js
+++ data-storage/db.js
@@ -1,15 +1,11 @@
 const MongoClient = require('mongodb').MongoClient;
-const url = "mongodb://localhost:27017/mydb";
+const url = "mongodb://localhost:27017/";
 
-MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+exports = MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
   if (err) throw err;
-  const dbo = db.db("vehicles");
+  let dbo = db.db("vehicles");
 
-  dbo.createCollection("testbus1", function(err, res) {
-    if (err) throw err;
-    console.log("Collection created!");
-    db.close();
-  });
+  console.log("Database connected");
 
   // dbo.collection("testbus1").drop(function(err, delOK) {
   //   if (err) throw err;
--- data-storage/server.js
+++ data-storage/server.js
@@ -1,62 +1,64 @@
-const fs = require ( "fs")
-const {Server} = require ('http')
 const express = require('express')
-const  IO = require('socket.io')
+const bodyParser = require('body-parser')
+const router = express.Router();
 
+const socket = require('socket.io')
+
+const mongodb = require("mongodb");
 const MongoClient = require('mongodb').MongoClient;
-const url = "mongodb://localhost:27017/mydb";
-
-// const Koa = require('koa')
-// const  {useKoaServer} = require ("routing-controllers")
+const url = "mongodb://localhost:27017";
 
 const NATS = require("nats")
 const nats = NATS.connect({url: "nats://localhost:4222", json: true})
 
 const port = process.env.PORT || 4000
-// const app = new Koa()
+
 const app = express()
-const server = new Server()
+app.use(bodyParser.json());
 
-// app.get('/vehicles', (req, res) => {  
-//   House.findAll().then(houses => {
-//     res.json({ houses: houses }) 
-//   })
-//   .catch(err => {
-//     res.status(500).json({
-//       message: 'Something went wrong',
-//       error: err
-//     })
-//   })
-// })
+let server = app.listen(port, () => console.log(`Listening on port ${port}`))
 
-// const server = new Server(app.callback())
 
-// useKoaServer(app, {
-//   cors: true,
-//   controllers: []
-// })
-
-// const io = IO(server)
-
-//   io.on('connect', () => {
-
-//     console.log(`User just connected`)
-  
-//     socket.on('disconnect', () => {
-//       console.log(`User just disconnected`)
-//     })
-//   })
 
 MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
-  if (err) throw err;
-  const dbo = db.db("vehicles");
+  if(err) return console.error(err);
+
+  let dbo = db.db("vehicles");
 
   nats.subscribe('vehicle.test-bus-1', (message) => 
-  
-  dbo.collection("testbus1").insertOne(message, function(err, res) {
-    if (err) throw err;
-    console.log("1 document inserted");
+    dbo.collection("testbus1").insertOne(message, function(err, res) {
+      if (err) throw err;
+      console.log("1 document inserted");
   }))
+
+  const io = socket(server)
+
+  io.on('connection', () => {
+
+    console.log(`Socket connected`)
+  
+    socket.on('disconnect', () => {
+      console.log(`Socket disconnected`)
+    })
+  })
+
+  // socket.emit('output', res)
+
+ router.get('/', (req, res) => {
+    return dbo.collection('testbus1').find().limit(100).toArray( (err, docs) => {
+      if(err) return next(err);
+
+      if(docs){
+        res.send(docs)
+      } else {
+        res.send({message: 'Vehicle data does not exist'})
+      }
+    })
+  });
+
 });
 
-server.listen(port, () => console.log(`Listening on port ${port}`))
+app.use('/', router)
+
+
+
