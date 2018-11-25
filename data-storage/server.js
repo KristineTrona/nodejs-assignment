5d3c898dc29da7f01cf4565909a938ab952c0fac
Kristine <kristinetrona@gmail.com>
2018-11-25 12:38:55 +0100

    post and get requests working

--- data-storage/server.js
+++ data-storage/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const router = express.Router();
+const request = require('superagent')
 
 const socket = require('socket.io')
 
@@ -12,53 +13,63 @@ const NATS = require("nats")
 const nats = NATS.connect({url: "nats://localhost:4222", json: true})
 
 const port = process.env.PORT || 4000
+const baseUrl = 'http://localhost:4000'
 
 const app = express()
 app.use(bodyParser.json());
 
 let server = app.listen(port, () => console.log(`Listening on port ${port}`))
 
+nats.subscribe('vehicle.test-bus-1', (message) => 
 
+  request
+  .post(`${baseUrl}/`)
+  .type('json')
+  .send(message)
+  .catch(console.error)
+)
 
 MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
   if(err) return console.error(err);
 
   let dbo = db.db("vehicles");
 
-  nats.subscribe('vehicle.test-bus-1', (message) => 
-    dbo.collection("testbus1").insertOne(message, function(err, res) {
-      if (err) throw err;
-      console.log("1 document inserted");
-  }))
+  // const io = socket(server)
 
-  const io = socket(server)
+  // io.on('connection', () => {
 
-  io.on('connection', () => {
-
-    console.log(`Socket connected`)
+  //   console.log(`Socket connected`)
   
-    socket.on('disconnect', () => {
-      console.log(`Socket disconnected`)
-    })
-  })
+  //   socket.on('disconnect', () => {
+  //     console.log(`Socket disconnected`)
+  //   })
+  // })
 
   // socket.emit('output', res)
 
- router.get('/', (req, res) => {
-    return dbo.collection('testbus1').find().limit(100).toArray( (err, docs) => {
+ router.get(`/`, (req, res) => {
+    return dbo.collection('testbus1').find().toArray( (err, docs) => {
       if(err) return next(err);
 
       if(docs){
         res.send(docs)
+        console.log(docs.length)
       } else {
         res.send({message: 'Vehicle data does not exist'})
       }
     })
   });
 
+  router.post(`/`, (req, res) => {
+    return dbo.collection("testbus1").insertOne(req.body, function(err, res) {
+      if (err) throw err;
+      console.log("1 document inserted");
+    })
+  });
+
 });
 
-app.use('/', router)
+app.use(`/`, router)
 
 
 
---
+++ front-end/src/components/Map.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default class Map extends React.Component {
+  state = {}
+
+
+
+  render() {
+    return(
+      <div>
+        Test
+      </div>
+    )
+  }
+}
